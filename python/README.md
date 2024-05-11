44b70b78b8d30841f6f69fa6edcb5e1845028549
nafanz <nafanz@mail.ru>
2024-05-11 13:09:38 +0500

    Updating versions, formatting code

--- python/README.md
+++ python/README.md
@@ -1,5 +1,6 @@
 # Phython based framework
 
 Steps to install
-1. Run ```pip install -r requirements.txt```
-2. With the installation complete run ```py.test```
+1. Install dependencies ```pip install -r requirements.txt```
+2. Update package manager ```pip install --upgrade pip```
+3. With the installation complete run ```py.test```
--- python/api/booking.py
+++ python/api/booking.py
@@ -1,21 +1,43 @@
 import requests
 
+url_booking = 'https://restful-booker.herokuapp.com/booking'
+
 
 def all_bookings():
-    return requests.get('https://restful-booker.herokuapp.com/booking')
+    return requests.get(url=url_booking)
 
 
 def specific_booking(identifier, accept):
-    headers = {'Accept': accept}
-    return requests.get('https://restful-booker.herokuapp.com/booking/' + str(identifier), headers=headers)
+    headers = {
+        'Accept': accept
+    }
+
+    return requests.get(
+        url=f'{url_booking}/{identifier}',
+        headers=headers
+    )
 
 
 def create_booking(payload, content_type):
-    headers = {'Accept': 'application/json', 'Content-type': content_type}
-    return requests.post('https://restful-booker.herokuapp.com/booking', payload, headers=headers)
+    headers = {
+        'Accept': 'application/json',
+        'Content-type': content_type
+    }
+
+    return requests.post(
+        url_booking,
+        payload,
+        headers=headers
+    )
 
 
 def delete_booking(identifier, token):
-    headers = {'Cookie': 'token=' + token}
-    return requests.delete('https://restful-booker.herokuapp.com/booking/' + str(identifier), headers=headers)
+    headers = {
+        'Cookie': f'token={token}'
+    }
+
+    return requests.delete(
+        url=f'{url_booking}/{identifier}',
+        headers=headers
+    )
 
